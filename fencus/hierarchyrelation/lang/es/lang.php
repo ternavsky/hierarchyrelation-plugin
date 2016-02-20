45cc13c1e2f5d4d13e29b67bf9854b659b8d9237
FlingeR <marianielias@gmail.com>
2016-02-20 12:08:18 -0300

    First Upload

---
+++ fencus/hierarchyrelation/lang/es/lang.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'plugin' => [
+        'name' => 'Fencus Hierarchy Relation Manager',
+        'description' => 'Una solucion para manejar Relaciones Jerárquicas.',
+    ],
+	'errors' => [
+		'wrong_relation' => "La Relacion ':relation' en el Modelo ':class' es del tipo ':type', Fencus Hierarchy Relation solo soporta ':needed'.",
+	]
+];
